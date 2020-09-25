1085ae063efa935968ba5317f351cdc8c57fe499
Sanjay PJ <sanjaypjayan2000@gmail.com>
2020-09-26 04:25:05 +0530

    .

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * C program to create and include custom header file
+ */
+
+#include <stdio.h>
+#include "arith.h"   // Include our custom header file
+
+int main () {
+	printf("sum(10, 20)  = %.2f\n", sum(10, 20));
+	printf("sub(10, 20)  = %.2f\n", sub(10, 20));
+	printf("div(10, 20)  = %.2f\n", div(10, 20));
+	printf("mul(10, 20)  = %.2f\n", mult(10, 20));
+	printf("mod(10, 20)  = %d\n", mod(10, 20));
+
+	return 0;
+}
